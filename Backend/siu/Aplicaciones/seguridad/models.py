c8f622772ffd77bbaa18848a1fb24569358b8f52
Sebas23Jack <156771837+Sebas23Jack@users.noreply.github.com>
2026-09-23 12:37:41 -0500

    Update models.py

--- Backend/siu/Aplicaciones/seguridad/models.py
+++ Backend/siu/Aplicaciones/seguridad/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from Backend.siu.Aplicaciones.basemodel.models import BaseModel
 
-# Create your models here.
+class Genero(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class EstadoCivil(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class GrupoSanguineo(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
