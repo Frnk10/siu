e49875e03a60f1028750c12cc1f7f19f2015ec96
Frnk10 <franciscoporras1000@gmail.com>
2026-09-23 23:47:47 -0500

    Creacion de tablas para Ventas y Seguridad

--- Backend/siu/Aplicaciones/seguridad/models.py
+++ Backend/siu/Aplicaciones/seguridad/models.py
@@ -30,6 +30,16 @@ class Instruccion(BaseModel):
     def __str__(self):
         return self.nombre
 
+class Localidad(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    codigo_padre = models.IntegerField(blank=True, null=True)
+    codigo_auxiliar = models.CharField(max_length=25, blank=True, null=True)
+    nombre = models.CharField(max_length=250, blank=False, null=False)
+    nivel = models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return self.nombre
+
 class Persona(BaseModel):
     # Relación Uno a Uno con el User de Django
     user = models.OneToOneField(
@@ -39,28 +49,48 @@ class Persona(BaseModel):
         related_name='persona'        # Permite acceder desde user via: request.user.persona
     )
     codigo = models.AutoField(primary_key=True)
-    fk_codigo_genero = models.ForeignKey(Genero)
-    fk_codigo_estado_civil = models.ForeignKey(EstadoCivil)
-    fk_codigo_grupo_sanguineo = models.ForeignKey(GrupoSanguineo)
-    fk_codigo_instruccion = models.ForeignKey(Instruccion)
-    identidficacion = models.CharField(max_length=13)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    razon_social = models.CharField(max_length=100)
-    fecha_nacimiento = models.DateField()
-    fotografia = models.BinaryField(null=True,blank=True)
-    mail1 = models.EmailField(max_length=50)
-    mail2 = models.EmailField(max_length=50)
-    telefono1 = models.CharField(max_length=15)
-    telefono2 = models.CharField(max_length=15)
-    celular1 = models.CharField(max_length=15)
-    celular2 = models.CharField(max_length=15)
-    calle_principal = models.CharField(max_length=50)
-    calle_secundaria = models.CharField(max_length=50)
-    numero_casa = models.CharField(max_length=10)
-    tipo = models.CharField(max_length=10)
-    ubicacion = models.CharField(max_length=100)
-    estado = models.CharField(max_length=10)
+    pk_codigo_genero = models.ForeignKey(
+        Genero,
+        db_column='pk_codigo_genero',
+        related_name='codigo_generos'
+    )
+    pk_codigo_estado_civil = models.ForeignKey(
+        EstadoCivil,
+        db_column='pk_codigo_estado_civil',
+        related_name='codigo_estado_civil'
+    )
+    pk_codigo_grupo_sanguineo = models.ForeignKey(
+        GrupoSanguineo,
+        db_column='pk_codigo_grupo_sanguineo',
+        related_name='codigo_grupo_sanguineo'
+    )
+    pk_codigo_instruccion = models.ForeignKey(
+        Instruccion,
+        db_column='pk_codigo_instruccion',
+        related_name='codigo_instruccion'
+    )
+    pk_codigo_localidad = models.ForeignKey(
+        Localidad,
+        db_column='pk_codigo_localidad',
+        related_name='codigo_localidad'
+    )
+    identificacion = models.CharField(max_length=13, blank=False, null=False)
+    razon_social = models.CharField(max_length=100, blank=True, null=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    fotografia = models.BinaryField(blank=True, null=True)
+    mail1 = models.EmailField(max_length=50, blank=True, null=True)
+    mail2 = models.EmailField(max_length=50, blank=True, null=True)
+    telefono1 = models.CharField(max_length=9, blank=True, null=True)
+    telefono2 = models.CharField(max_length=9, blank=True, null=True)
+    celular1 = models.CharField(max_length=10, blank=True, null=True)
+    celular2 = models.CharField(max_length=10, blank=True, null=True)
+    calle_principal = models.CharField(max_length=50, blank=True, null=True)
+    calle_secundaria = models.CharField(max_length=50, blank=True, null=True)
+    numero_casa = models.CharField(max_length=50, blank=True, null=True)
+    tipo = models.CharField(
+        max_length=1, help_text="P=PERSONA NATURAL, S=SOCIEDAD", blank=False, null=False
+    )
+    ubicacion = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
@@ -132,22 +162,81 @@ class Sucursal(BaseModel):
     def __str__(self):
         return self.nombre
 
-class Usuario(Persona):
-    fk_codigo_sucursal = models.ForeignKey(Sucursal)
+class Usuario(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    pk_codigo_sucursal = models.ForeignKey(
+        Sucursal,
+        db_column='pk_codigo_sucursal',
+        related_name='codigo_sucursal'
+    )
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=300)
 
     def __str__(self):
         return self.username
 
+class Rol(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    codigo_auxiliar = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+    permisos = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+class Menu(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    codigo_padre = models.IntegerField(blank=True, null=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200, blank=True, null=True)
+    url = models.CharField(max_length=200, blank=True, null=True)
+    nivel = models.IntegerField()
+    orden = models.IntegerField()
+    icono = models.CharField(max_length=50, blank=True, null=True)
+    estilo_clase = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class RolMenu(models.Model):
+    pk_codigo_rol = models.ForeignKey(
+        Rol,
+        db_column='pk_codigo_rol',
+        related_name='codigo_rol'
+    )
+    pk_codigo_menu = models.ForeignKey(
+        Menu,
+        db_column='pk_codigo_menu',
+        related_name='codigo_menu'
+    )
+    
+class UsuarioRol(models.Model):
+    pk_codigo_usuario = models.ForeignKey(
+        Usuario,
+        db_column='pk_codigo_usuario',
+        related_name='codigo_usuarios'
+    )
+    pk_codigo_rol = models.ForeignKey(
+        Rol,
+        db_column='pk_codigo_rol',
+        related_name='codigo_rol'
+    )
+
 class IngresoSistema():
     codigo = models.AutoField(primary_key=True)
-    fk_codigo_usuario = models.ForeignKey(Usuario)
-    fecha = models.DateTimeField(auto_now_add=True)
-    descripcion = models.CharField(max_length=200)
-    ip = models.CharField(max_length=25)
-    validador = models.CharField(max_length=25)
-    estado = models.CharField(default="G", max_length=1, help_text="G=GENERADO, O=OCUPADO, C=CADUCADO")
+    pk_codigo_usuario = models.ForeignKey(
+        Usuario,
+        db_column='pk_codigo_usuario',
+        related_name='codigo_usuarios'
+    )
+    fecha = models.DateTimeField(auto_now_add=True, auto_now=True)
+    descripcion = models.CharField(max_length=200, blank=True, null=True)
+    ip = models.CharField(max_length=25, blank=True, null=True)
+    validador = models.CharField(max_length=25, blank=True, null=True)
+    estado = models.CharField(
+        default="G", max_length=1, help_text="G=GENERADO, O=OCUPADO, C=CADUCADO"
+    )
 
     def __str__(self):
         return self.codigo
+
--- Backend/siu/Aplicaciones/venta/models.py
+++ Backend/siu/Aplicaciones/venta/models.py
@@ -1,3 +1,143 @@
 from django.db import models
+from Backend.siu.Aplicaciones.basemodel.models import BaseModel
 
 # Create your models here.
+
+class Cliente(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    tiene_credito = models.CharField(
+        default="N", max_length=1, help_text="S=SI, N=NO", blank=False, null=False
+    )
+    dias_mora = models.IntegerField(blank=True, null=True)
+    monto_credito = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    saldo_credito = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    deuda_total = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    tipo = models.CharField(
+        default="N", max_length=1, help_text="N=NORMAL, M=MAYORISTA", blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.tiene_credito
+
+class Comprobante(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    pk_codigo_cliente = models.ForeignKey(
+        Cliente,
+        db_column='pk_codigo_cliente',
+        related_name='codigo_clientes'
+    )
+    id_comprobante_referencia = models.IntegerField(blank=True, null=True)
+    fecha_emision = models.DateTimeField(blank=False, null=False)
+    clave_acceso = models.CharField(max_length=49, blank=False, null=False)
+    numero_autorizacion = models.CharField(max_length=49, blank=True, null=True)
+    estado_transmision = models.CharField(
+        default="GEN", max_length=3, 
+        help_text="GEN=GENERADO, TRA=TRANSMITIDO, AUT=AUTORIZADO, NAT=NO AUTORIZADO", 
+        blank=False, null=False
+    )
+    fecha_respuesta = models.DateField(blank=True, null=True)
+    tipo_comprobante = models.CharField(
+        default="01", max_length=2, 
+        help_text="01=FACTURA, 03=LIQUIDACION COMPRA, 04=NOTA CREDITO, 05=NOTA DEBITO, 06=GUIA REMISION, 07=GUIA REMISION", 
+        blank=False, null=False
+    )
+    tipo_ambiente = models.CharField(
+        default="1", max_length=1, help_text="1=PRUEBAS, 2=PRODUCCION", blank=False, null=False
+    )
+    serie = models.CharField(max_length=6, blank=False, null=False)
+    secuencial = models.CharField(max_length=9, blank=False, null=False)
+    codigo_numerico = models.CharField(max_length=8, blank=False, null=False)
+    tipo_emision = models.CharField(
+        default="1", max_length=1, help_text="1=NORMAL", blank=False, null=False
+    )
+    digito_verificador = models.CharField(max_length=1, blank=False, null=False)
+    subtotal = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    impuesto1 = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    impuesto2 = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    descuento = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    total = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    tipo = models.CharField(
+        default="C", max_length=1, help_text="C=COMPROBANTE, P=PROFORMA", blank=False, null=False
+    )
+    estado_comprobante = models.CharField(
+        max_length=1, help_text="G=GENERADO, P=PAGADO, D=DESPACHADO", blank=False, null=False
+    )
+
+    def __str__(self):
+        return self.fecha_emision
+
+class ComprobanteDetalle(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    pk_codigo_cabecera = models.ForeignKey(
+        Cliente,
+        db_column='pk_codigo_cabecera',
+        related_name='codigo_cabeceras'
+    )
+    id_producto = models.IntegerField(blank=False, null=False)
+    id_promocion = models.IntegerField(blank=True, null=True)
+    nombre_producto = models.CharField(max_length=250, blank=False, null=False)
+    numero_serie = models.CharField(max_length=200, blank=True, null=True)
+    cantidad = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    precio_compra = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    precio_unidad = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    descuento = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    total = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    tipo_precio = models.CharField(
+        default="C", max_length=1, 
+        help_text="C=CLIENTE, D=DISTRIBUIDOR, P=PROMOCION", blank=False, null=False
+    )
+
+    def __str__(self):
+        return self.nombre_producto
+
+class Cobro(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    pk_codigo_comprobante = models.ForeignKey(
+        Comprobante,
+        db_column='pk_codigo_comprobante',
+        related_name='codigo_comprobantes'
+    )
+    forma = models.CharField(
+        default="C", max_length=1, help_text="C=CONTADO, D=DIFERIDO", blank=False, null=False
+    )
+    fecha = models.DateField(blank=False, null=False)
+    total_comprobante = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    numero_cuotas = models.IntegerField(blank=False, null=False)
+    porcentaje_intereses = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    entrada = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    monto_cuota = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    total = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    estado_cobro = models.CharField(
+        max_length=1, help_text="G=GENERADO, P=PENDIENTE, C=COMPLETADO", blank=False, null=False
+    )
+
+    def __str__(self):
+        return self.total_comprobante
+
+class CobroDetalle(BaseModel):
+    codigo = models.AutoField(primary_key=True)
+    pk_codigo_cobro= models.ForeignKey(
+        Cobro,
+        db_column='pk_codigo_cobro',
+        related_name='codigo_cobros'
+    )
+    id_movimiento_caja = models.IntegerField(blank=True, null=True)
+    numero_cuota = models.IntegerField(blank=False, null=False)
+    fecha_cuota = models.DateField(blank=False, null=False)
+    monto_cuota = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    fecha_cobro = models.DateField(blank=True, null=True)
+    monto_cobro_efectivo = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    monto_cobro_transferencia = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    monto_cobro_cheque = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    monto_cobro_comprobante = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    id_comprobante = models.IntegerField(blank=True, null=True)
+    monto_cobro_tarjeta = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    tipo_tarjeta = models.CharField(
+        max_length=1, help_text="C=CREDITO, D=DEBITO", blank=True, null=True
+    )
+    monto_cobro_total = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+    archivo1 = models.BinaryField(blank=True, null=True)
+    archivo1 = models.BinaryField(blank=True, null=True)
+
+    def __str__(self):
+        return self.numero_cuota
